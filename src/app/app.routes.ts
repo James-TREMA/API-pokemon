e44882ee87c50547b1ef789e02742d85abde649d
James-TREMA <andru29@live.fr>
2024-10-23 17:38:51 +0200

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'pokemons', component: PokemonListComponent },
+  { path: '', redirectTo: '/pokemons', pathMatch: 'full' }
+];
